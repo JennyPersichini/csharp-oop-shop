6b6fa5d7862cc8b84badbca4d3b35447386403d9
JennyPersichini <nenny1096@gmail.com>
2023-04-18 16:15:49 +0200

    Create prodotto

--- csharp-oop-shop/Prodotto.cs
+++ csharp-oop-shop/Prodotto.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace csharp_oop_shop
 {
@@ -15,37 +17,51 @@ namespace csharp_oop_shop
         private double Prezzo;
         private double Iva;
 
-        public Prodotto(int Codice, string Nome, string Descrizione, double Prezzo, double Iva)
+        public Prodotto(string Nome, string Descrizione, double Prezzo, double Iva)
         {
-                this.Codice = CreaProdotto();
-                this.Nome = Nome;
-                this.Descrizione = Descrizione;
-                this.Prezzo = Prezzo;
-                this.Iva = Iva;
-            }
+            Codice = CreaProdotto();
+
+            this.Nome = Nome;
+            this.Descrizione = Descrizione;
+            this.Prezzo = Prezzo;
+            this.Iva = Iva;
+        }
+
         //Alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random
+        //Il codice prodotto sia accessibile solo in lettura
+        //Gli altri attributi siano accessibili sia in lettura che in scrittura
+        //Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva
+        //Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice + nome
 
         private int CreaProdotto()
         {
             Random NumeroRandom = new Random();
             return NumeroRandom.Next();
         }
-
-        //Il codice prodotto sia accessibile solo in lettura
-
-
-
-        //Gli altri attributi siano accessibili sia in lettura che in scrittura
-
-
-
-        //Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva
-
-
-
-        //Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice + nome
-
-
-
+        
+        public string getNome()
+        {
+            return Nome;
+        }
+        
+        public string getDescrizione()
+        {
+            return Descrizione;
+        }
+        
+        public double getPrezzo()
+        {
+            return Prezzo;
+        }
+        
+        public double getPrezzoConIva()
+        {
+            return Prezzo + (Prezzo / 100 * Iva);
+        }
+        
+        public string getNomeEsteso()
+        {
+            return Codice.ToString() + " " + Nome;
+        }
     }
 }
