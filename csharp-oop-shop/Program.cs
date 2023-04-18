56ec6e414de05c7cf9af06baa4fe6ba9ef1fe6c9
JennyPersichini <nenny1096@gmail.com>
2023-04-18 16:22:31 +0200

    fix program

--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
+
+using csharp_oop_shop;
+
+Prodotto p1 = new Prodotto("Sedia", "Sedia pieghevole da giardino", 15, 10);
+
+//Console.WriteLine("Codice Prodotto: {0}", p1.getCodiceProdotto());
+
+Console.WriteLine("Nome: {0}", p1.getNome());
+Console.WriteLine("Descrizione: {0}", p1.getDescrizione());
+Console.WriteLine("Prezzo: {0}", p1.getPrezzo());
+Console.WriteLine("Prezzo + iva: {0}", p1.getPrezzoConIva());
+Console.WriteLine("Nome completo: {0}", p1.getNomeEsteso());
+Console.WriteLine("-----------------------------------");
+
