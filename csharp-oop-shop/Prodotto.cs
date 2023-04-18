468c349e9fde2dd00b9067b9023782258d3c4b6f
JennyPersichini <nenny1096@gmail.com>
2023-04-18 15:38:08 +0200

    Create RandomNumber

--- csharp-oop-shop/Prodotto.cs
+++ csharp-oop-shop/Prodotto.cs
@@ -15,11 +15,37 @@ namespace csharp_oop_shop
         private double Prezzo;
         private double Iva;
 
-
+        public Prodotto(int Codice, string Nome, string Descrizione, double Prezzo, double Iva)
+        {
+                this.Codice = CreaProdotto();
+                this.Nome = Nome;
+                this.Descrizione = Descrizione;
+                this.Prezzo = Prezzo;
+                this.Iva = Iva;
+            }
         //Alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random
+
+        private int CreaProdotto()
+        {
+            Random NumeroRandom = new Random();
+            return NumeroRandom.Next();
+        }
+
         //Il codice prodotto sia accessibile solo in lettura
+
+
+
         //Gli altri attributi siano accessibili sia in lettura che in scrittura
+
+
+
         //Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva
+
+
+
         //Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice + nome
+
+
+
     }
 }
