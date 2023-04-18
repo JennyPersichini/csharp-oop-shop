f7e7ce773fc2c35375f2c1867a70b23455044f9f
JennyPersichini <nenny1096@gmail.com>
2023-04-18 16:54:30 +0200

    Add Bonus folder

---
+++ csharp-oop-shop_bonus/Prodotto.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    internal class Prodotto
+    {
+        private int Codice { get; }
+        private string Nome { get; set; }
+        private string Descrizione { get; set; }
+        private double Prezzo { get; set; }
+        private double Iva { get; set; }
+
+        public Prodotto(string Nome, string Descrizione, double Prezzo, double Iva)
+        {
+            Codice = CreaProdotto();
+
+            this.Nome = Nome;
+            this.Descrizione = Descrizione;
+            this.Prezzo = Prezzo;
+            this.Iva = Iva;
+        }
+
+        private int CreaProdotto()
+        {
+            Random NumeroRandom = new Random();
+            return NumeroRandom.Next();
+        }
+
+        public string getNome()
+        {
+            return Nome;
+        }
+
+        public string getDescrizione()
+        {
+            return Descrizione;
+        }
+
+        public double getPrezzo()
+        {
+            return Prezzo;
+        }
+
+        public double getPrezzoConIva()
+        {
+            return Prezzo + (Prezzo / 100 * Iva);
+        }
+
+        public string getNomeEsteso()
+        {
+            return Codice.ToString() + " " + Nome;
+        }
+    }
+}
---
+++ csharp-oop-shop_bonus/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+Prodotto p1 = new Prodotto("Sedia", "Sedia pieghevole da giardino", 15, 10);
+
+//Console.WriteLine("Codice Prodotto: {0}", p1.getCodiceProdotto());
+
+Console.WriteLine("Nome: {0}", p1.getNome());
+Console.WriteLine("Descrizione: {0}", p1.getDescrizione());
+Console.WriteLine("Prezzo: {0}", p1.getPrezzo());
+Console.WriteLine("Prezzo + iva: {0}", p1.getPrezzoConIva());
+Console.WriteLine("Nome completo: {0}", p1.getNomeEsteso());
+Console.WriteLine("-----------------------------------");
+
+Prodotto p2 = new Prodotto("Tavolo", "Tavolo in ferro battuto da giardino", 30, 15);
+
+Console.WriteLine("Nome: {0}", p2.getNome());
+Console.WriteLine("Descrizione: {0}", p2.getDescrizione());
+Console.WriteLine("Prezzo: {0}", p2.getPrezzo());
+Console.WriteLine("Prezzo + iva: {0}", p2.getPrezzoConIva());
+Console.WriteLine("Nome completo: {0}", p2.getNomeEsteso());
+Console.WriteLine("-----------------------------------");
+
+Prodotto p3 = new Prodotto("Gazebo", "Gazebo in ferro battuto", 150, 20);
+
+Console.WriteLine("Nome: {0}", p3.getNome());
+Console.WriteLine("Descrizione: {0}", p3.getDescrizione());
+Console.WriteLine("Prezzo: {0}", p3.getPrezzo());
+Console.WriteLine("Prezzo + iva: {0}", p3.getPrezzoConIva());
+Console.WriteLine("Nome completo: {0}", p3.getNomeEsteso());
+Console.WriteLine("-----------------------------------");
