e811ad5c7bdc4629f1b9fd476028b252e9ca8b73
JennyPersichini <nenny1096@gmail.com>
2023-04-18 16:44:18 +0200

    Add set/get

--- csharp-oop-shop/Prodotto.cs
+++ csharp-oop-shop/Prodotto.cs
@@ -7,15 +7,21 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
+//Alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random
+//Il codice prodotto sia accessibile solo in lettura
+//Gli altri attributi siano accessibili sia in lettura che in scrittura
+//Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva
+//Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice + nome
+
 namespace csharp_oop_shop
 {
     internal class Prodotto
     {
-        private int Codice;
-        private string Nome;
-        private string Descrizione;
-        private double Prezzo;
-        private double Iva;
+        private int Codice { get; }
+        private string Nome { get; set; }
+        private string Descrizione { get; set; }
+        private double Prezzo { get; set; }
+        private double Iva { get; set; }
 
         public Prodotto(string Nome, string Descrizione, double Prezzo, double Iva)
         {
@@ -26,12 +32,6 @@ namespace csharp_oop_shop
             this.Prezzo = Prezzo;
             this.Iva = Iva;
         }
-
-        //Alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random
-        //Il codice prodotto sia accessibile solo in lettura
-        //Gli altri attributi siano accessibili sia in lettura che in scrittura
-        //Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva
-        //Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice + nome
 
         private int CreaProdotto()
         {
